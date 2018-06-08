563d75528844f62a05d2936379d2cda30c172796
jnizet <jb@ninja-squad.com>
2018-06-08 11:33:55 +0200

    build: make the asciidoctor task repeatable safely
    The asciidoctor sources are not all parts of the sourceDir of the task. So if we modify one of the .adoc files and rerun the task without cleaning first, the task is marked as up-to-date. This fixes the issue by adding all the .adoc files as inputs of the task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,9 +64,14 @@ subprojects {
 }
 
 // See CONTRIBUTING.adoc in order to have Kotlin syntax highlighting
-tasks.withType<AsciidoctorTask> {
-	sourceDir = File("src/docs/asciidoc")
-	outputDir = File("build/docs")
+tasks {
+	val asciidoctor by getting(AsciidoctorTask::class) {
+		sourceDir = File("src/docs/asciidoc")
+		outputDir = File("build/docs")
+		inputs.files(fileTree("${projectDir}") {
+			include("**/*.adoc")
+		})
+	}
 }
 
 dependencies {
