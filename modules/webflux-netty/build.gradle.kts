d26a5061120e4eff87eac25374f1fa5bc4b0b6aa
Alexey Nesterov <alexey.nesterov@live.com>
2018-06-14 22:49:16 +0100

    Migrate webflux-tomcat to use WebServerModuleTest

--- modules/webflux-netty/build.gradle.kts
+++ modules/webflux-netty/build.gradle.kts
@@ -4,6 +4,8 @@ dependencies {
 	implementation("io.projectreactor.netty:reactor-netty")
 
 	testImplementation(project(":modules:webflux-test-common"))
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testImplementation("org.springframework:spring-test")
 
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
