45b9f446f3316d2eddc70512d3d2a8b65da32e04
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-07-04 15:14:01 +0200

    Upgrade to Kotlin 1.2.51

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.2.50" apply false
+	id("org.jetbrains.kotlin.jvm") version "1.2.51" apply false
 	id("org.springframework.boot") version "2.0.3.RELEASE" apply false
 	id("io.spring.dependency-management") version "1.0.5.RELEASE"
 	id("org.asciidoctor.convert") version "1.5.6" apply false
