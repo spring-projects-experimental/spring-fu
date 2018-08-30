5cd39280a5f34fbacb3487b23a22e21d00e480d9
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-08-30 11:02:47 +0200

    Fix bootstrap build order

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,3 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd spring-fu
 ./gradlew -q build --stacktrace
-cd bootstraps/coroutine-webapp
-./gradlew -q build --stacktrace
-cd ../reactive-webapp
-./gradlew -q build --stacktrace
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,3 +5,7 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd spring-fu
 ./gradlew -q -PrepoUsername=$ARTIFACTORY_USERNAME -PrepoPassword=$ARTIFACTORY_PASSWORD publish
+cd bootstraps/coroutine-webapp
+./gradlew -q build --stacktrace
+cd ../reactive-webapp
+./gradlew -q build --stacktrace
