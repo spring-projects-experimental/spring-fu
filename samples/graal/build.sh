221d43938372b767aadf9f52e03df787b603c96c
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-09-04 10:50:23 +0200

    Fix Graal sample build

--- samples/graal/build.sh
+++ samples/graal/build.sh
@@ -2,6 +2,6 @@
 
 cd ../../
 ./gradlew clean build -x test
-cd samples/graal-webapp
-unzip build/libs/graal-webapp-0.0.1.BUILD-SNAPSHOT.jar -d build/libs/graal-webapp-0.0.1.BUILD-SNAPSHOT
-native-image -H:ReflectionConfigurationFiles=graal.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/graal-webapp-0.0.1.BUILD-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/graal-webapp-0.0.1.BUILD-SNAPSHOT/BOOT-INF/classes org.springframework.fu.sample.graal.ApplicationKt
+cd samples/graal
+unzip build/libs/graal-0.0.1.BUILD-SNAPSHOT.jar -d build/libs/graal-0.0.1.BUILD-SNAPSHOT
+native-image -H:ReflectionConfigurationFiles=graal.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/graal-0.0.1.BUILD-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/graal-0.0.1.BUILD-SNAPSHOT/BOOT-INF/classes org.springframework.fu.sample.graal.ApplicationKt
