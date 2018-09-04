25ec3d3db99df2020093e6db565d2992fac20a24
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-09-04 11:13:17 +0200

    Fix deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,7 +5,7 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd spring-fu
 ./gradlew -q -PrepoUsername=$ARTIFACTORY_USERNAME -PrepoPassword=$ARTIFACTORY_PASSWORD publish
-cd bootstraps/coroutine-webapp
+cd bootstraps/coroutines
 ./gradlew -q build --stacktrace
-cd ../reactive-webapp
+cd ../reactive
 ./gradlew -q build --stacktrace
