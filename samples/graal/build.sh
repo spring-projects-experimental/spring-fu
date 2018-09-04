8f4b3a8c8b4bc68da6a781be5977f0a6ccde6938
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-09-04 11:16:33 +0200

    Add Jetty Graal configuration

--- samples/graal/build.sh
+++ samples/graal/build.sh
@@ -4,4 +4,4 @@ cd ../../
 ./gradlew clean build -x test
 cd samples/graal
 unzip build/libs/graal-0.0.1.BUILD-SNAPSHOT.jar -d build/libs/graal-0.0.1.BUILD-SNAPSHOT
-native-image -H:ReflectionConfigurationFiles=graal.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/graal-0.0.1.BUILD-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/graal-0.0.1.BUILD-SNAPSHOT/BOOT-INF/classes org.springframework.fu.sample.graal.ApplicationKt
+native-image -H:ReflectionConfigurationFiles=graal-netty.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/graal-0.0.1.BUILD-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/graal-0.0.1.BUILD-SNAPSHOT/BOOT-INF/classes org.springframework.fu.sample.graal.ApplicationKt
