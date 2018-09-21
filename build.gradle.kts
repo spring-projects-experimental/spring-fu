8c52dd41e65928857bd51ed025d35d97e570d199
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-09-21 10:45:52 +0200

    Upgrade to Kotlin 1.3 RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.3-M2"
+	id("org.jetbrains.kotlin.jvm") version "1.3.0-rc-57"
 	id("org.springframework.boot") version "2.1.0.BUILD-SNAPSHOT" apply false
 	id("io.spring.dependency-management") version "1.0.6.RELEASE"
 	id("maven-publish")
@@ -58,6 +58,7 @@ allprojects {
 		maven("https://repo.spring.io/milestone")
 		maven("https://repo.spring.io/snapshot")
 		maven("http://dl.bintray.com/kotlin/kotlin-eap")
+		maven("https://jcenter.bintray.com")
 	}
 
 	dependencyManagement {
