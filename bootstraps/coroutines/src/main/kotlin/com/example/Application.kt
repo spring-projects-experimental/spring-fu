4beef9e5ec7388773af0e6a1823b10ac7f88405f
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-09-25 07:55:15 -0400

    Fix bootstraps

--- bootstraps/coroutines/src/main/kotlin/com/example/Application.kt
+++ bootstraps/coroutines/src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,11 @@
 package com.example
 
 import org.springframework.boot.kofu.application
-import org.springframework.boot.kofu.web.netty
 import org.springframework.boot.kofu.web.server
-import org.springframework.boot.kofu.web.coRouter
 
 val app = application {
-	val port = if (profiles.contains("test")) 8181 else 8080
-	server(netty(port)) {
+	server {
+		port = if (profiles.contains("test")) 8181 else 8080
 		coRouter {
 			GET("/") { ok().syncBody("Hello world!") }
 		}
--- bootstraps/reactive/build.gradle.kts
+++ bootstraps/reactive/build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.kotlin.dsl.version
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.3-M2"
+	id("org.jetbrains.kotlin.jvm") version "1.3.0-rc-57"
 	id("io.spring.dependency-management") version "1.0.6.RELEASE"
-	id("org.springframework.boot") version "2.1.0.BUILD-SNAPSHOT"
+	id("org.springframework.boot") version "2.1.0.M4"
 }
 
 dependencies {
