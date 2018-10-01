a747b8ac4d224ca598398ae834eac53d15c2d895
ksvasan <vk.srinivas@gmail.com>
2018-10-01 20:26:50 +0530

    Remove all the Resource related imports

--- fuconfigure/src/main/java/org/springframework/boot/autoconfigure/web/reactive/FormCodecInitializer.java
+++ fuconfigure/src/main/java/org/springframework/boot/autoconfigure/web/reactive/FormCodecInitializer.java
@@ -18,15 +18,12 @@ package org.springframework.boot.autoconfigure.web.reactive;
 
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.codec.ResourceDecoder;
-import org.springframework.core.io.Resource;
 import org.springframework.http.codec.CodecConfigurer;
 import org.springframework.http.codec.FormHttpMessageReader;
 import org.springframework.http.codec.FormHttpMessageWriter;
-import org.springframework.http.codec.ResourceHttpMessageWriter;
 
 /**
- * {@link ApplicationContextInitializer} adapter for registering {@link Resource} codecs.
+ * {@link ApplicationContextInitializer} adapter for registering Form codecs.
  */
 public class FormCodecInitializer extends AbstractCodecInitializer {
 
