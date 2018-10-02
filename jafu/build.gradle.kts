5420cae70dcc61b53e6c1af03ec90362b7ea7699
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-02 16:40:54 +0200

    Publish spring-boot-jafu artifact

--- jafu/build.gradle.kts
+++ jafu/build.gradle.kts
@@ -6,3 +6,23 @@ dependencies {
 
 	compileOnly("org.springframework:spring-webflux")
 }
+
+publishing {
+	publications {
+		create(project.name, MavenPublication::class.java) {
+			from(components["java"])
+			artifactId = "spring-boot-jafu"
+			val sourcesJar by tasks.creating(Jar::class) {
+				classifier = "sources"
+				from(sourceSets["main"].allSource)
+			}
+			artifact(sourcesJar)
+			val javadocJar by tasks.creating(Jar::class) {
+				dependsOn("javadoc")
+				classifier = "javadoc"
+				from(buildDir.resolve("dokka"))
+			}
+			artifact(javadocJar)
+		}
+	}
+}
