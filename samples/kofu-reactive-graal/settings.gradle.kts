87f28e51b01d686900c37c1048a096e5ce7ab4fb
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-08 18:31:34 +0200

    Add missing snapshot dependencies and repositories

--- samples/kofu-reactive-graal/settings.gradle.kts
+++ samples/kofu-reactive-graal/settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://repo.spring.io/milestone")
+        maven("https://repo.spring.io/snapshot")
         maven("http://dl.bintray.com/kotlin/kotlin-eap")
     }
     resolutionStrategy {
--- samples/settings.gradle.kts
+++ samples/settings.gradle.kts
@@ -14,6 +14,7 @@ pluginManagement {
 	repositories {
 		gradlePluginPortal()
 		maven("https://repo.spring.io/milestone")
+		maven("https://repo.spring.io/snapshot")
 		maven("http://dl.bintray.com/kotlin/kotlin-eap")
 	}
 	resolutionStrategy {
