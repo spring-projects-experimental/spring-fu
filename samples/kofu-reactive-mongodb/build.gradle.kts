ada7459bcf02fdb538e6bdf39099566987ff248f
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-08 12:07:17 +0200

    Upgrade to Kotlin 1.3.0-RC3

--- samples/kofu-reactive-mongodb/build.gradle.kts
+++ samples/kofu-reactive-mongodb/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.kotlin.dsl.version
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.3.0-rc-116"
+	id("org.jetbrains.kotlin.jvm") version "1.3.0-rc-146"
 	id("io.spring.dependency-management") version "1.0.6.RELEASE"
 	id("org.springframework.boot") version "2.1.0.M4"
 }
