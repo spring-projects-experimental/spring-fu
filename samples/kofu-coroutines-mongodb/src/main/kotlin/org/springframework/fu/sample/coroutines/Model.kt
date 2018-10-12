4917a654ee6aca650f3b5d215f98d7836f319e03
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-12 19:30:24 +0200

    Add missing @Document annotations

--- samples/kofu-coroutines-mongodb/src/main/kotlin/org/springframework/fu/sample/coroutines/Model.kt
+++ samples/kofu-coroutines-mongodb/src/main/kotlin/org/springframework/fu/sample/coroutines/Model.kt
@@ -1,7 +1,9 @@
 package org.springframework.fu.sample.coroutines
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document
 data class User(
 	@Id val login: String,
 	val firstname: String,
--- samples/kofu-reactive-mongodb/src/main/kotlin/com/sample/Model.kt
+++ samples/kofu-reactive-mongodb/src/main/kotlin/com/sample/Model.kt
@@ -1,7 +1,9 @@
 package com.sample
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document
 data class User(
 	@Id val login: String,
 	val firstname: String,
