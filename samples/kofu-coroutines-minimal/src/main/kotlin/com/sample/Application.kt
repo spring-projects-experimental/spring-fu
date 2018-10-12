a112fdced0f325ff70b90078bc8c57105512e056
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-12 07:22:23 +0200

    Inlined routers in minimal samples

--- samples/kofu-coroutines-minimal/src/main/kotlin/com/sample/Application.kt
+++ samples/kofu-coroutines-minimal/src/main/kotlin/com/sample/Application.kt
@@ -2,16 +2,13 @@ package com.sample
 
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.web.server
-import org.springframework.web.function.server.coRouter
-
-val router = coRouter {
-	GET("/") { ok().syncBody("Hello world!") }
-}
 
 val app = application {
 	server {
 		port = if (profiles.contains("test")) 8181 else 8080
-		import(router)
+		coRouter {
+			GET("/") { ok().syncBody("Hello world!") }
+		}
 	}
 }
 
--- samples/kofu-reactive-minimal/src/main/kotlin/com/sample/Application.kt
+++ samples/kofu-reactive-minimal/src/main/kotlin/com/sample/Application.kt
@@ -2,16 +2,13 @@ package com.sample
 
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.web.server
-import org.springframework.web.reactive.function.server.router
-
-val router = router {
-	GET("/") { ok().syncBody("Hello world!") }
-}
 
 val app = application {
 	server {
 		port = if (profiles.contains("test")) 8181 else 8080
-		import(router)
+		router {
+			GET("/") { ok().syncBody("Hello world!") }
+		}
 	}
 }
 
