fc3849cfb7d6f8eff97c5eca3e1fce4b72ff664f
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-18 18:04:00 +0200

    Upgrade to Kotlin 1.3.0-RC4

--- samples/kofu-reactive-graal/build.gradle.kts
+++ samples/kofu-reactive-graal/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.3.0-rc-146"
+	id("org.jetbrains.kotlin.jvm") version "1.3.0-rc-190"
 	id("io.spring.dependency-management") version "1.0.6.RELEASE"
 	id("org.springframework.boot") version "2.1.0.RC1"
 }
