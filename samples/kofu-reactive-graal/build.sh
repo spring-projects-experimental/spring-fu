88d0d456330618cddc6d18862ba23e6dae1dda48
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-10-28 11:12:58 +0100

    Polish GraalVM sample

--- samples/kofu-reactive-graal/build.sh
+++ samples/kofu-reactive-graal/build.sh
@@ -3,8 +3,4 @@
 ./gradlew clean build
 unzip build/libs/kofu-reactive-graal.jar -d build/libs/kofu-reactive-graal
 
-# Netty
-native-image --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,org.springframework.core.io.VfsUtils -H:ReflectionConfigurationFiles=graal-netty.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/kofu-reactive-graal/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/kofu-reactive-graal/BOOT-INF/classes com.sample.ApplicationKt
-
-# Jetty
-#native-image -H:ReflectionConfigurationFiles=graal-jetty.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/kofu-reactive-graal/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/kofu-reactive-graal/BOOT-INF/classes com.sample.ApplicationKt
+native-image --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,org.springframework.core.io.VfsUtils -H:ReflectionConfigurationFiles=graal.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/kofu-reactive-graal/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/kofu-reactive-graal/BOOT-INF/classes com.sample.ApplicationKt
