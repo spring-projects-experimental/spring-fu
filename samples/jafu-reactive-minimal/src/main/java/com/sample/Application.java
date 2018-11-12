40e2223b1823e2838b7c8f2312a385816908a9e1
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-11-12 11:44:14 +0100

    Add missing jafu-reactive-minimal Application.java file

---
+++ samples/jafu-reactive-minimal/src/main/java/com/sample/Application.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import static org.springframework.fu.jafu.ApplicationDsl.*;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.fu.jafu.ApplicationDsl;
+
+public class Application {
+
+	public static ApplicationDsl app = application(app -> app.beans(beans -> beans
+			.bean(SampleService.class)
+			.bean(SampleHandler.class)).server(server -> server.router(router -> {
+		SampleHandler sampleHandler = app.ref(SampleHandler.class);
+		router
+				.GET("/", sampleHandler::hello)
+				.resources("/**", new ClassPathResource("static/"));
+	})));
+
+	public static void main (String[] args) {
+		app.run(args);
+	}
+}
