f3fbbfd3040249e22248e5aad57e54d73f3c22b5
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-11-21 17:54:37 +0100

    Polish Jafu samples

--- samples/jafu-reactive-minimal/src/main/java/com/sample/Application.java
+++ samples/jafu-reactive-minimal/src/main/java/com/sample/Application.java
@@ -7,14 +7,17 @@ import org.springframework.fu.jafu.ApplicationDsl;
 
 public class Application {
 
-	public static ApplicationDsl app = application(app -> app.beans(beans -> beans
+	public static ApplicationDsl app = application(app ->
+		app.beans(beans -> beans
 			.bean(SampleService.class)
-			.bean(SampleHandler.class)).server(server -> server.router(router -> {
-		var sampleHandler = app.ref(SampleHandler.class);
-		router
-				.GET("/", sampleHandler::hello)
-				.resources("/**", new ClassPathResource("static/"));
-	})));
+			.bean(SampleHandler.class))
+			.server(server -> server.router(router -> {
+				var sampleHandler = app.ref(SampleHandler.class);
+				router.GET("/", sampleHandler::hello)
+					.resources("/**", new ClassPathResource("static/"));
+			})
+		)
+	);
 
 	public static void main (String[] args) {
 		app.run(args);
