0172cacbe72b1fcfe5dc246cd34de1db9af016c1
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-12-05 17:26:33 +0100

    Update README

--- samples/jafu-reactive-minimal/src/main/java/com/sample/Application.java
+++ samples/jafu-reactive-minimal/src/main/java/com/sample/Application.java
@@ -2,23 +2,17 @@ package com.sample;
 
 import static org.springframework.fu.jafu.ApplicationDsl.*;
 import static org.springframework.fu.jafu.web.WebFluxServerDsl.server;
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.fu.jafu.ApplicationDsl;
 
 public class Application {
 
-	public static ApplicationDsl app = application(app ->
-		app.beans(beans -> beans
-			.bean(SampleService.class)
-			.bean(SampleHandler.class))
-			.enable(server(s -> s.router(router -> {
-				var sampleHandler = app.ref(SampleHandler.class);
-				router.GET("/", sampleHandler::hello)
-					.resources("/**", new ClassPathResource("static/"));
-			})
-		))
-	);
+	public static ApplicationDsl app = application(a ->
+			a.enable(server(s -> s.router(r ->
+							r.GET("/", request -> ok().syncBody("Hello world!"))
+					))
+			));
 
 	public static void main (String[] args) {
 		app.run(args);
