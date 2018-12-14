25663e7c364cab8d59f32ff12bac57f55ad94cbf
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-12-14 15:29:50 +0100

    Upgrade Boot to 2.1.1

--- samples/kofu-reactive-r2dbc/build.gradle.kts
+++ samples/kofu-reactive-r2dbc/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.kotlin.dsl.version
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.3.10"
+	id("org.jetbrains.kotlin.jvm") version "1.3.11"
 	id("io.spring.dependency-management") version "1.0.6.RELEASE"
 	id("org.springframework.boot") version "2.1.1.RELEASE"
 }
