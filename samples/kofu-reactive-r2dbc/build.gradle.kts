5eb7dd176ecf3bba30a4ffaf7e4b238f7331d433
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-05-13 14:37:55 +0200

    Upgrade to Kotlin 1.3.31

--- samples/kofu-reactive-r2dbc/build.gradle.kts
+++ samples/kofu-reactive-r2dbc/build.gradle.kts
@@ -2,7 +2,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.3.21"
+	id("org.jetbrains.kotlin.jvm") version "1.3.31"
 	id("io.spring.dependency-management") version "1.0.7.RELEASE"
 	id("org.springframework.boot") version "2.2.0.BUILD-SNAPSHOT"
 }
