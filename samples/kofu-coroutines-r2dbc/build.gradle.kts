f340439202e4be42dd82899524a0a80f29c3b374
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-05-14 11:09:05 +0200

    Fix R2DBC transitive dependencies

--- samples/kofu-coroutines-r2dbc/build.gradle.kts
+++ samples/kofu-coroutines-r2dbc/build.gradle.kts
@@ -11,19 +11,17 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.data:spring-data-r2dbc:1.0.0.BUILD-SNAPSHOT")
+	implementation("org.springframework.data:spring-data-r2dbc:1.0.0.M2")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.2.1")
-	implementation("io.r2dbc:r2dbc-h2") {
-		exclude(module = "reactor-netty")
-	}
+	implementation("io.r2dbc:r2dbc-h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
 	imports {
-		mavenBom("io.r2dbc:r2dbc-bom:Arabba-BUILD-SNAPSHOT")
+		mavenBom("io.r2dbc:r2dbc-bom:Arabba-M8")
 	}
 }
 
