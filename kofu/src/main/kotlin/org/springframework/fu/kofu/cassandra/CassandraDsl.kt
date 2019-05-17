3a734a87f72b5ddc79f0a31c306b399274eff069
Stefan Andjelkovic <sa.stefan.andjelkovic@gmail.com>
2019-05-17 19:01:59 +0200

    Add remaining Cassandra properties

--- kofu/src/main/kotlin/org/springframework/fu/kofu/cassandra/CassandraDsl.kt
+++ kofu/src/main/kotlin/org/springframework/fu/kofu/cassandra/CassandraDsl.kt
@@ -1,11 +1,14 @@
 package org.springframework.fu.kofu.cassandra
 
+import com.datastax.driver.core.ConsistencyLevel
+import com.datastax.driver.core.ProtocolOptions
 import org.springframework.boot.autoconfigure.cassandra.CassandraInitializer
 import org.springframework.boot.autoconfigure.cassandra.CassandraProperties
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataInitializer
 import org.springframework.context.support.GenericApplicationContext
 import org.springframework.fu.kofu.AbstractDsl
 import org.springframework.fu.kofu.ConfigurationDsl
+import java.time.Duration
 
 /**
  * Kofu DSL for Cassandra configuration.
@@ -75,6 +78,76 @@ open class CassandraDsl(private val init: CassandraDsl.() -> Unit) : AbstractDsl
 		set(value) {
 			properties.isSsl = value
 		}
+
+	/**
+	 * Configure the compression supported by the Cassandra binary protocol.
+	 */
+	var compression: ProtocolOptions.Compression?
+		get() = properties.compression
+		set(value) {
+			properties.compression = value
+		}
+
+	/**
+	 * Configure the queries consistency level.
+	 */
+	var consistencyLevel: ConsistencyLevel?
+		get() = properties.consistencyLevel
+		set(value) {
+			properties.consistencyLevel = value
+		}
+
+	/**
+	 * Configure the queries serial consistency level.
+	 */
+	var serialConsistencyLevel: ConsistencyLevel?
+		get() = properties.serialConsistencyLevel
+		set(value) {
+			properties.serialConsistencyLevel = value
+		}
+
+	/**
+	 * Configure the queries default fetch size.
+	 */
+	var fetchSize: Int
+		get() = properties.fetchSize
+		set(value) {
+			properties.fetchSize = value
+		}
+
+	/**
+	 * Configure the socket option: connection time out.
+	 */
+	var connectTimeout: Duration?
+		get() = properties.connectTimeout
+		set(value) {
+			properties.connectTimeout = value
+		}
+
+	/**
+	 * Configure the socket option: read time out.
+	 */
+	var readTimeout: Duration?
+		get() = properties.readTimeout
+		set(value) {
+			properties.readTimeout = value
+		}
+
+	/**
+	 * Configure whether to enable JMX reporting. Default to false as Cassandra JMX reporting is not
+	 * compatible with Dropwizard Metrics.
+	 */
+	var jmxEnabled: Boolean
+		get() = properties.isJmxEnabled
+		set(value) {
+			properties.isJmxEnabled = value
+		}
+
+	/**
+	 * Retrieve the pool configuration.
+	 */
+	val pool: CassandraProperties.Pool?
+		get() = properties.pool
 
 	override fun initialize(context: GenericApplicationContext) {
 		super.initialize(context)
--- kofu/src/test/kotlin/org/springframework/fu/kofu/samples/cassandra.kt
+++ kofu/src/test/kotlin/org/springframework/fu/kofu/samples/cassandra.kt
@@ -1,9 +1,12 @@
 package org.springframework.fu.kofu.samples
 
+import com.datastax.driver.core.ConsistencyLevel
+import com.datastax.driver.core.ProtocolOptions
 import org.springframework.boot.WebApplicationType
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.cassandra.cassandra
 import org.springframework.fu.kofu.cassandra.reactiveCassandra
+import java.time.Duration
 
 fun cassandra() {
 	application(WebApplicationType.NONE) {
@@ -15,6 +18,13 @@ fun cassandra() {
 			port = 9042
 			ssl = false
 			contactPoints.add("localhost")
+			compression = ProtocolOptions.Compression.NONE
+			consistencyLevel = ConsistencyLevel.ANY
+			serialConsistencyLevel = ConsistencyLevel.SERIAL
+			connectTimeout = Duration.ofSeconds(1)
+			fetchSize = 5000
+			readTimeout = Duration.ofSeconds(2)
+			jmxEnabled = false
 		}
 	}
 }
@@ -29,6 +39,13 @@ fun reactiveCassandra() {
 			port = 9042
 			ssl = false
 			contactPoints.add("localhost")
+			compression = ProtocolOptions.Compression.NONE
+			consistencyLevel = ConsistencyLevel.ANY
+			serialConsistencyLevel = ConsistencyLevel.SERIAL
+			connectTimeout = Duration.ofSeconds(1)
+			fetchSize = 5000
+			readTimeout = Duration.ofSeconds(2)
+			jmxEnabled = false
 		}
 	}
 }
