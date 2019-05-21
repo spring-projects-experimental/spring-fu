a4c185b2b29afbc33cfc3f7973e4532b0a8d7d59
Jonas Bark <jonas.bark@ffuf.de>
2019-05-21 08:53:42 +0200

    add support for MSSQL + r2dbc

---
+++ kofu/src/main/kotlin/org/springframework/fu/kofu/r2dbc/MssqlR2dbcDsl.kt
@@ -0,0 +1,86 @@
+package org.springframework.fu.kofu.r2dbc
+
+import org.springframework.boot.autoconfigure.data.r2dbc.MssqlDatabaseClientInitializer
+import org.springframework.boot.autoconfigure.data.r2dbc.MssqlR2dbcProperties
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.fu.kofu.AbstractDsl
+import org.springframework.fu.kofu.ConfigurationDsl
+import java.time.Duration
+
+/**
+ * Kofu DSL for R2DBC MSSQL configuration.
+ *
+ * Enable and configure R2DBC support by registering a [org.springframework.data.r2dbc.core.DatabaseClient] bean.
+ *
+ * Required dependencies are `io.r2dbc:r2dbc-mssql` and `org.springframework.data:spring-data-r2dbc`.
+ *
+ * @sample org.springframework.fu.kofu.samples.r2dbcMssql
+ * @author Jonas Bark
+ */
+class MssqlR2dbcDsl(private val init: MssqlR2dbcDsl.() -> Unit) : AbstractDsl() {
+
+    /**
+     * Configure the host, by default set to `localhost`.
+     */
+    var host: String = "localhost"
+
+    /**
+     * Configure the port, by default set to `5432`.
+     */
+    var port: Int = 5432
+
+    /**
+     * Configure the database, by default set to `postgres`.
+     */
+    var database: String = "postgres"
+
+    /**
+     * Configure the username, by default set to `postgres`.
+     */
+    var username: String = "postgres"
+
+    /**
+     * Configure the password, empty by default.
+     */
+    var password: String = ""
+
+    /**
+     * Configure whether to prefer cursored execution.
+     */
+    var preferCursoredExecution: Boolean = false
+
+    /**
+     * Configure the connect timeout. Defaults to 30 seconds.
+     */
+    var connectTimeout: Duration = Duration.ofSeconds(30)
+
+    /**
+     * Enable or disable SSL usage. This flag is also known as Use Encryption in other drivers.
+     */
+    var ssl: Boolean = false
+
+    override fun initialize(context: GenericApplicationContext) {
+        super.initialize(context)
+        init()
+
+        val properties = MssqlR2dbcProperties()
+        properties.host = host
+        properties.port = port
+        properties.database = database
+        properties.username = username
+        properties.password = password
+        properties.isPreferCursoredExecution = preferCursoredExecution
+        properties.connectTimeout = connectTimeout
+        properties.isSsl = ssl
+
+        MssqlDatabaseClientInitializer(properties).initialize(context)
+    }
+}
+
+/**
+ * Configure R2DBC MSSQL support.
+ * @see MssqlR2dbcDsl
+ */
+fun ConfigurationDsl.r2dbcMssql(dsl: MssqlR2dbcDsl.() -> Unit = {}) {
+    MssqlR2dbcDsl(dsl).initialize(context)
+}
