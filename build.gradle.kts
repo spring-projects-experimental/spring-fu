69430ddec1f3c2be7979719418d40ffea0bdca2a
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-09-05 12:04:22 +0200

    Upgrade to dependency plugin 1.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.3.50" apply false
 	id("org.springframework.boot") apply false
 	id("org.jetbrains.dokka") version "0.9.18" apply false
-	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	id("maven-publish")
 }
 
--- samples/kofu-reactive-minimal/build.gradle.kts
+++ samples/kofu-reactive-minimal/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.3.50"
-	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	id("org.springframework.boot") version "2.2.0.BUILD-SNAPSHOT"
 }
 
