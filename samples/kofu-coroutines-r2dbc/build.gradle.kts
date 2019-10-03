79a5d42beb0fce3f94c40488d762d5f14270245d
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-10-03 14:33:44 +0200

    Upgrade to Spring Boot 2.2.0.RC1
    Closes gh-249

--- samples/kofu-coroutines-r2dbc/build.gradle.kts
+++ samples/kofu-coroutines-r2dbc/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.3.50"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	id("org.springframework.boot") version "2.2.0.BUILD-SNAPSHOT"
+	id("org.springframework.boot") version "2.2.0.RC1"
 }
 
 dependencies {
@@ -11,7 +11,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.data:spring-data-r2dbc:1.0.0.BUILD-SNAPSHOT")
+	implementation("org.springframework.data:spring-data-r2dbc:1.0.0.RC1")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("io.r2dbc:r2dbc-h2")
@@ -21,7 +21,7 @@ dependencies {
 
 dependencyManagement {
 	imports {
-		mavenBom("io.r2dbc:r2dbc-bom:Arabba-BUILD-SNAPSHOT")
+		mavenBom("io.r2dbc:r2dbc-bom:Arabba-RC1")
 	}
 }
 
--- samples/kofu-reactive-r2dbc/build.gradle.kts
+++ samples/kofu-reactive-r2dbc/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.3.50"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	id("org.springframework.boot") version "2.2.0.BUILD-SNAPSHOT"
+	id("org.springframework.boot") version "2.2.0.RC1"
 }
 
 dependencies {
@@ -12,7 +12,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.data:spring-data-r2dbc:1.0.0.BUILD-SNAPSHOT")
+	implementation("org.springframework.data:spring-data-r2dbc:1.0.0.RC1")
 	implementation("io.r2dbc:r2dbc-h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -20,7 +20,7 @@ dependencies {
 
 dependencyManagement {
 	imports {
-		mavenBom("io.r2dbc:r2dbc-bom:Arabba-BUILD-SNAPSHOT")
+		mavenBom("io.r2dbc:r2dbc-bom:Arabba-RC1")
 	}
 }
 
