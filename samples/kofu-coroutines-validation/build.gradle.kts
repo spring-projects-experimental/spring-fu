dde75d7011c5c8064445afeb1ad7b1e8a062ffb5
Sébastien Deleuze <sdeleuze@pivotal.io>
2019-11-12 15:31:51 +0100

    Upgrade to Spring Boot 2.2.1 and Gradle 5.6.4
    Closes gh-254

--- samples/kofu-coroutines-validation/build.gradle.kts
+++ samples/kofu-coroutines-validation/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.3.50"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	id("org.springframework.boot") version "2.2.0.RELEASE"
+	id("org.springframework.boot") version "2.2.1.RELEASE"
 }
 
 dependencies {
