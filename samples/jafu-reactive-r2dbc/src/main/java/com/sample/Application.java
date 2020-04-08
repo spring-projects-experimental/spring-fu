76cb951b8a840b282a1a24e2330126d5d5a0619b
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-08 15:14:59 +0200

    Use fluent syntax in Jafu examples
    Uses gh-267

--- samples/jafu-reactive-r2dbc/src/main/java/com/sample/Application.java
+++ samples/jafu-reactive-r2dbc/src/main/java/com/sample/Application.java
@@ -8,11 +8,10 @@ import static org.springframework.fu.jafu.Jafu.application;
 
 public abstract class Application {
 
-	public static JafuApplication app = application(WebApplicationType.REACTIVE, app ->
-		app.enable(Configurations.dataConfig)
-		   .enable(Configurations.webConfig)
-		   .listener(ApplicationReadyEvent.class, e -> app.ref(UserRepository.class).init())
-	);
+	public static JafuApplication app = application(WebApplicationType.REACTIVE, app -> app
+				.enable(Configurations.dataConfig)
+				.enable(Configurations.webConfig)
+				.listener(ApplicationReadyEvent.class, e -> app.ref(UserRepository.class).init()));
 
 	public static void main (String[] args) {
 		app.run(args);
--- samples/jafu-reactive-r2dbc/src/main/java/com/sample/Configurations.java
+++ samples/jafu-reactive-r2dbc/src/main/java/com/sample/Configurations.java
@@ -9,11 +9,11 @@ import static org.springframework.fu.jafu.webflux.WebFluxServerDsl.webFlux;
 
 public abstract class Configurations {
 
-	public static Consumer<ConfigurationDsl> dataConfig = conf ->
-			conf.beans(beans -> beans.bean(UserRepository.class)).enable(r2dbcH2());
+	public static Consumer<ConfigurationDsl> dataConfig = conf -> conf
+			.beans(b -> b.bean(UserRepository.class)).enable(r2dbcH2());
 
-	public static Consumer<ConfigurationDsl> webConfig = conf ->
-			conf.beans(beans -> beans.bean(UserHandler.class))
+	public static Consumer<ConfigurationDsl> webConfig = conf -> conf
+			.beans(beans -> beans.bean(UserHandler.class))
 			.enable(webFlux(server -> {
 				if (conf.profiles().contains("test")) {
 					server.port(8181);
@@ -21,9 +21,8 @@ public abstract class Configurations {
 				else {
 					server.port(8080);
 				}
-				server.router(router -> {
-					var userHandler = conf.ref(UserHandler.class);
-					router.GET("/", userHandler::listApi);
-				}).codecs(codecs -> codecs.string().jackson());
+				server
+						.router(r -> r.GET("/", conf.ref(UserHandler.class)::listApi))
+						.codecs(codecs -> codecs.string().jackson());
 			}));
 }
