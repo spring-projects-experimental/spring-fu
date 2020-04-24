876c1cced09c75d48411c7b78702aed3a1bf0dfa
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-24 12:45:56 +0200

    Switch to 0.3.0.BUILD-SNAPSHOT

--- samples/kofu-reactive-minimal/build.gradle.kts
+++ samples/kofu-reactive-minimal/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-	implementation("org.springframework.fu:spring-fu-kofu:0.3.0.M3")
+	implementation("org.springframework.fu:spring-fu-kofu:0.3.0.BUILD-SNAPSHOT")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
