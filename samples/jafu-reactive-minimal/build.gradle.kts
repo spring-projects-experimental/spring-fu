e6e5474355e7cd1dca16d3c3394adbd11061ac48
Sébastien Deleuze <sdeleuze@vmware.com>
2020-05-27 11:45:54 +0200

    Upgrade to Spring Boot 2.3.0.RELEASE
    Closes gh-265

--- samples/jafu-reactive-minimal/build.gradle.kts
+++ samples/jafu-reactive-minimal/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	id("org.springframework.boot") version "2.3.0.RC1"
+	id("org.springframework.boot") version "2.3.0.RELEASE"
 	id("java")
 }
 
