f8ca53ebcf3b4ba9370f800be95f42dd87c89f40
Audrey Neveu <aneveu@pivotal.io>
2020-07-17 14:44:54 +0200

    remove H2 and Postgre configuration from Jafu + update Jafu Reactive R2dbc sample

--- samples/jafu-reactive-r2dbc/src/main/java/com/sample/Configurations.java
+++ samples/jafu-reactive-r2dbc/src/main/java/com/sample/Configurations.java
@@ -4,13 +4,13 @@ import java.util.function.Consumer;
 
 import org.springframework.fu.jafu.ConfigurationDsl;
 
-import static org.springframework.fu.jafu.r2dbc.H2R2dbcDsl.r2dbcH2;
+import static org.springframework.fu.jafu.r2dbc.R2dbcDsl.r2dbc;
 import static org.springframework.fu.jafu.webflux.WebFluxServerDsl.webFlux;
 
 public abstract class Configurations {
 
 	public static Consumer<ConfigurationDsl> dataConfig = conf -> conf
-			.beans(b -> b.bean(UserRepository.class)).enable(r2dbcH2());
+			.beans(b -> b.bean(UserRepository.class)).enable(r2dbc(dsl -> dsl.url("r2dbc:h2:mem:///testdb")));
 
 	public static Consumer<ConfigurationDsl> webConfig = conf -> conf
 			.beans(beans -> beans.bean(UserHandler.class))
