a676942bf0799cf43b7c88e18a1db00c89ce081e
Sébastien Deleuze <sdeleuze@vmware.com>
2020-08-04 11:21:27 +0200

    Fix Thymeleaf sample
    See gh-243

--- kofu/src/test/kotlin/org/springframework/fu/kofu/samples/thymeleaf.kt
+++ kofu/src/test/kotlin/org/springframework/fu/kofu/samples/thymeleaf.kt
@@ -1,12 +1,11 @@
 package org.springframework.fu.kofu.samples
 
-import org.springframework.boot.WebApplicationType
-import org.springframework.fu.kofu.application
+import org.springframework.fu.kofu.webApplication
 import org.springframework.fu.kofu.webflux.thymeleaf
 import org.springframework.fu.kofu.webflux.webFlux
 
 fun thymeleafDsl() {
-    application(WebApplicationType.REACTIVE) {
+    webApplication {
         webFlux {
             thymeleaf()
         }
