914d05c0eda43be19514aec704aff1474ab0baf9
Hamid Mortazavi <hamid.mortazavi@gmail.com>
2020-11-15 16:32:00 +0000

    Upgrade to Spring Boot 2.4.0

--- samples/jafu-servlet-cassandra/build.gradle.kts
+++ samples/jafu-servlet-cassandra/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	id("org.springframework.boot") version "2.4.0-RC1"
+	id("org.springframework.boot") version "2.4.0"
 	id("java")
 }
 
