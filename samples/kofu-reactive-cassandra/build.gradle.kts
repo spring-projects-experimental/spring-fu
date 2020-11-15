4d483ba771c26f44e516f86b17e14976643f7712
Hamid Mortazavi <hamid.mortazavi@gmail.com>
2020-11-15 16:32:00 +0000

    Upgrade to Spring Boot 2.4.0

--- samples/kofu-reactive-cassandra/build.gradle.kts
+++ samples/kofu-reactive-cassandra/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.4.10"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	id("org.springframework.boot") version "2.4.0-RC1"
+	id("org.springframework.boot") version "2.4.0"
 }
 
 dependencies {
