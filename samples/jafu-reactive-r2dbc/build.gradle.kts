bf6edc74094a2b5b7cf1a301b68525875dc0daba
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-06-12 17:54:14 +0200

    bump to spring-boot 2.5.1

--- samples/jafu-reactive-r2dbc/build.gradle.kts
+++ samples/jafu-reactive-r2dbc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("org.springframework.boot") version "2.5.0"
+	id("org.springframework.boot") version "2.5.1"
 	id("java")
 }
 
