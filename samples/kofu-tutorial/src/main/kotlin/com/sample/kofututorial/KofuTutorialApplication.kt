6c93f7be36cf4cd813b47b1e168cf76e4ae2da49
lucapiccinelli <luca.picci@gmail.com>
2022-02-22 07:48:21 +0100

    mustache activation explanation

--- samples/kofu-tutorial/src/main/kotlin/com/sample/kofututorial/KofuTutorialApplication.kt
+++ samples/kofu-tutorial/src/main/kotlin/com/sample/kofututorial/KofuTutorialApplication.kt
@@ -1,10 +1,22 @@
 package com.sample.kofututorial
 
+import org.springframework.fu.kofu.templating.mustache
 import org.springframework.fu.kofu.webApplication
 import org.springframework.fu.kofu.webmvc.webMvc
+import org.springframework.web.servlet.function.ServerResponse
 
 val app = webApplication {
     webMvc {
+        mustache{
+            prefix = "classpath:/views/"
+        }
+
+        router {
+            GET("/"){
+                ServerResponse.ok()
+                    .render("blog", mapOf("title" to "Blog"))
+            }
+        }
     }
 }
 
