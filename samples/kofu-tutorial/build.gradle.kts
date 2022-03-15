021cd06da1da009da772d8805d9ed6a9697aae9d
lucapiccinelli <luca.picci@gmail.com>
2022-03-15 08:02:54 +0100

    blog engine tests

--- samples/kofu-tutorial/build.gradle.kts
+++ samples/kofu-tutorial/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.1.0")
+    testImplementation("io.mockk:mockk:1.12.2")
 }
 
 tasks.withType<KotlinCompile> {
--- samples/kofu-tutorial/src/test/kotlin/com/sample/blog/BlogTests.kt
+++ samples/kofu-tutorial/src/test/kotlin/com/sample/blog/BlogTests.kt
@@ -1,11 +1,15 @@
 package com.sample.blog
 
+import io.mockk.every
+import io.mockk.mockk
 import org.hamcrest.Matchers
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.fu.kofu.webApplication
+import org.springframework.fu.kofu.webmvc.webMvc
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -16,11 +20,32 @@ class BlogTests {
     lateinit var context: ConfigurableApplicationContext
     lateinit var client: MockMvc
 
+    val app = webApplication {
+        enable(blog)
+
+        beans {
+            bean {
+                val author = UserEntity(Id(0), User.of("springluca", "Luca", "Piccinelli"))
+                val article = ArticleEntity(Id(0),
+                    Article(
+                        title = "Reactor Bismuth is out",
+                        headline = "Lorem ipsum",
+                        content = "dolor sit amet",
+                        authorFn = { author }
+                    )
+                )
+                mockk<ArticleRepository> {
+                    every { findAllByOrderByAddedAtDesc() } returns listOf(article)
+                    every { findBySlug(article.info.slug) } returns article
+                }
+            }
+        }
+        webMvc { port = (10000..10500).random() }
+    }
+
     @BeforeAll
     internal fun setUp() {
-        val port = (10000..10500).random()
-
-        context = app.run(arrayOf("--spring.port=${port}"))
+        context = app.run()
         client = MockMvcBuilders
             .webAppContextSetup(context as WebApplicationContext)
             .build()
@@ -38,6 +63,22 @@ class BlogTests {
                 status { isOk() }
                 content {
                     string(Matchers.containsString("<h1>Blog</h1>"))
+                    string(Matchers.containsString("Reactor"))
+                }
+            }
+    }
+
+    @Test
+    internal fun `Assert article page title, content and status code`() {
+        val title = "Reactor Bismuth is out"
+
+        client.get("/article/${title.toSlug()}")
+            .andExpect {
+                status { isOk() }
+                content {
+                    string(Matchers.containsString(title))
+                    string(Matchers.containsString("Lorem ipsum"))
+                    string(Matchers.containsString("dolor sit amet"))
                 }
             }
     }
